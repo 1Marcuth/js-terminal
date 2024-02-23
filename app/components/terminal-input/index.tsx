23059302cce1e5f90a41adebfe81a77040c2b169
Marcuth <thiago.playgames15@gmail.com>
2024-02-22 21:07:11 -0300

    Subindo últimas atualizações

--- app/components/terminal-input/index.tsx
+++ app/components/terminal-input/index.tsx
@@ -3,19 +3,17 @@ import { FC, ChangeEvent } from "react"
 import styles from "./style.module.scss"
 
 interface IProps {
-    onInputChange: (event: ChangeEvent<HTMLInputElement>) => any 
+    onInputChange: (event: ChangeEvent<HTMLInputElement>) => any
+    value: string
 }
 
-const TerminalInput: FC<IProps> = ({ onInputChange }) => {
-    const handleInputChange = () => {
-
-    }
-
+const TerminalInput: FC<IProps> = ({ onInputChange, value }) => {
     return (
         <div className={`${styles["terminal-input"]} flex w-full`}>
             <input
                 type="text"
                 onInput={onInputChange}
+                value={value}
                 className="w-full focus:outline-none bg-transparent"
             />
         </div>
--- app/components/terminal/index.tsx
+++ app/components/terminal/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { FC, ChangeEvent, useRef, useState, useEffect, createContext, useContext } from "react"
+import * as clientHelper from "client-helper"
 import { v4 as uuidv4 } from "uuid"
 
 import TerminalPrefix from "../terminal-prefix"
@@ -13,21 +14,7 @@ import styles from "./style.module.scss"
 
 
 function stringifyValue(value: any): string {
-    if (typeof value === "object") {
-        let properties
-
-        if (Array.isArray(value)) {
-            properties = value.map(stringifyValue).join(", ")
-        } else {
-            properties = Object.keys(value).map(function(key) {
-                return key + ": " + stringifyValue(value[key])
-            }).join(", ")
-        }
-
-        return Array.isArray(value) ? "[" + properties + "]" : "{" + properties + "}"
-    } else {
-        return value ? value.toString() : "undefined"
-    }
+    return value ? value.toString() : "undefined"
 }
 
 const CommandsContext = createContext<any>({})
@@ -64,7 +51,22 @@ type TerminalHistory = TerminalRowState[]
 
 type ExtendedWindow = {
     clear?: () => string
-} & Window & typeof globalThis
+} & Window & typeof globalThis & {
+    createAudio: typeof clientHelper.createAudio
+    createImage: typeof clientHelper.createImage
+    createVideo: typeof clientHelper.createVideo
+    fileToDataUrl: typeof clientHelper.fileToDataUrl
+    formatFileSize: typeof clientHelper.formatFileSize
+    getFileExtension: typeof clientHelper.getFileExtension
+    includeCss: typeof clientHelper.includeCss
+    includeScript: typeof clientHelper.includeScript
+    openAudio: typeof clientHelper.openAudio
+    openFile: typeof clientHelper.openFile
+    openImage: typeof clientHelper.openImage
+    openVideo: typeof clientHelper.openVideo
+    saveMedia: typeof clientHelper.saveMedia
+    saveTextFile: typeof clientHelper.saveTextFile
+}
 
 const getWindow = (): ExtendedWindow => {
     // @ts-ignore
@@ -74,6 +76,21 @@ const getWindow = (): ExtendedWindow => {
         console.clear()
         return "The console has been cleaned."
     }
+
+    extendedWindow.createAudio = clientHelper.createAudio
+    extendedWindow.createImage = clientHelper.createImage
+    extendedWindow.createVideo = clientHelper.createVideo
+    extendedWindow.fileToDataUrl = clientHelper.fileToDataUrl
+    extendedWindow.formatFileSize = clientHelper.formatFileSize
+    extendedWindow.getFileExtension = clientHelper.getFileExtension
+    extendedWindow.includeCss = clientHelper.includeCss
+    extendedWindow.includeScript = clientHelper.includeScript
+    extendedWindow.openAudio = clientHelper.openAudio
+    extendedWindow.openFile = clientHelper.openFile
+    extendedWindow.openImage = clientHelper.openImage
+    extendedWindow.openVideo = clientHelper.openVideo
+    extendedWindow.saveMedia = clientHelper.saveMedia
+    extendedWindow.saveTextFile = clientHelper.saveTextFile
 
     return extendedWindow
 }
@@ -85,12 +102,11 @@ const Terminal: FC = () => {
     const commandsContext = useContext(CommandsContext)
 
     const handleKeyEnterPressed = () => {
-        const currentCommand = command.trim();
-        if (currentCommand === "") return;
+        const currentCommand = command.trim()
 
         let output: string
 
-        if (currentCommand === "clear()") {
+        if (currentCommand === "clear()" || currentCommand === "console.clear()") {
             setHistory([])
         }
 
@@ -98,8 +114,17 @@ const Terminal: FC = () => {
 
         setHistory(prevHistory => [
             ...prevHistory,
-            { id: uuidv4(), cells: [{ id: uuidv4(), command: currentCommand, output }] },
-        ]);
+            {
+                id: uuidv4(),
+                cells: [
+                    {
+                        id: uuidv4(),
+                        command: currentCommand,
+                        output: output
+                    }
+                ]
+            }
+        ])
 
         setCommand("")
     }
@@ -129,7 +154,7 @@ const Terminal: FC = () => {
     return (
         <table 
             ref={tableRef}
-            className={`${styles["terminal"]} bg-gray-800 flex w-screen border rounded border-gray-600 px-2 py-3 w-4/5 min-w-[400px] max-w-[1000px] h-[600px] scroll-auto`}
+            className={`${styles["terminal"]} bg-gray-800 flex border rounded border-gray-600 px-2 py-3 w-4/5 min-w-[400px] max-w-[1000px] h-[600px] scroll-auto`}
         >
             <tbody className="w-full table">
                 {history.map(row => {
@@ -138,13 +163,13 @@ const Terminal: FC = () => {
                             {row.cells.map(cell => {
                                 return (
                                     <>
-                                        <TerminalCell key={cell.id}>
-                                            <TerminalPrefix prefix="$"/>
+                                        <TerminalCell key={`${cell.id}-a`}>
+                                            <TerminalPrefix prefix="$" key={`${cell.id}-a-prefix`}/>
                                             {cell.command}
                                         </TerminalCell>
-                                        <TerminalCell>
-                                            <TerminalOutput>
-                                                <i className="bi bi-arrow-return-right text-gray-600"/>
+                                        <TerminalCell key={`${cell.id}-b`}>
+                                            <TerminalOutput key={`${cell.id}-b-output`}>
+                                                <i className={`bi bi-arrow-return-right text-gray-600 ${cell.id}-b-icon`}/>
                                                 {cell.output}
                                             </TerminalOutput>
                                         </TerminalCell>
@@ -155,9 +180,13 @@ const Terminal: FC = () => {
                     )
                 })}
                 <TerminalRow>
-                    <TerminalCell>
-                        <TerminalPrefix prefix="$"/>
-                        <TerminalInput onInputChange={handleCommandChange}/>
+                    <TerminalCell key={"input-terminal-cell"}>
+                        <TerminalPrefix prefix="$" key="input-terminal-cell-prefix"/>
+                        <TerminalInput
+                            key="input-terminal-cell-input"
+                            onInputChange={handleCommandChange}
+                            value={command}
+                        />
                     </TerminalCell>
                 </TerminalRow>
             </tbody>
